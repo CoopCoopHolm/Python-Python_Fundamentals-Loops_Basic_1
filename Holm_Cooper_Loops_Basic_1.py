16a2375f343fae8b6452f28744849fbede2dba8f
CoopCoopHolm <78125160+CoopCoopHolm@users.noreply.github.com>
2021-03-15 11:29:54 -0500

    Add files via upload

---
+++ Holm_Cooper_Loops_Basic_1.py
@@ -0,0 +1,29 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range(5, 1001, 5):
+#     print(i)
+
+# for num in range(100):
+#     if num % 5 == 0:
+#         print("Coding")
+#     elif num % 10 == 0:
+#         print("Coding Dojo")
+
+# for num in range(500):
+#     if num % 2 == 1:
+#         sum = ()
+#         print(sum)
+
+# total = sum(range(0, 500000, 3))
+# print(total)
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+lowNum = 1
+highNum = 100
+mult = 3
+
+for i in range(lowNum, highNum, mult):
+        print(i)
